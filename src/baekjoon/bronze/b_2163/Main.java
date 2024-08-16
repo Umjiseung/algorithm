63110892a133d49a5b3aa829a8504b7f7ca3cd8c
Umjiseung <127853946+Umjiseung@users.noreply.github.com>
2024-08-16 16:48:17 +0900

    b_2163

---
+++ src/baekjoon/bronze/b_2163/Main.java
@@ -0,0 +1,21 @@
+package baekjoon.bronze.b_2163;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int n = Integer.parseInt(st.nextToken());
+        int m = Integer.parseInt(st.nextToken());
+
+        int p = n * m;
+        p--;
+
+        System.out.println(p);
+    }
+}
