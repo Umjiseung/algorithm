1e3c402daf4f517fb65c01492e01b0e52b053beb
Umjiseung <127853946+Umjiseung@users.noreply.github.com>
2024-09-15 15:46:57 +0900

    s_1822

--- src/baekjoon/silver/s_1822/Main.java
+++ src/baekjoon/silver/s_1822/Main.java
@@ -6,6 +6,7 @@ import java.io.InputStreamReader;
 import java.util.HashSet;
 import java.util.Set;
 import java.util.StringTokenizer;
+import java.util.TreeSet;
 
 public class Main {
     public static void main(String[] args) throws IOException {
@@ -15,45 +16,30 @@ public class Main {
         int a = Integer.parseInt(st.nextToken());
         int b = Integer.parseInt(st.nextToken());
 
-        int[] A = new int[a];
-        int[] B = new int[b];
+        Set<Integer> A = new HashSet<>();
+        Set<Integer> B = new HashSet<>();
 
         st = new StringTokenizer(br.readLine());
-        for (int i = 0; i < A.length; i++) {
-            A[i] = Integer.parseInt(st.nextToken());
+        for (int i = 0; i < a; i++) {
+            A.add(Integer.parseInt(st.nextToken()));
         }
 
         st = new StringTokenizer(br.readLine());
-        for (int i = 0; i < B.length; i++) {
-            B[i] = Integer.parseInt(st.nextToken());
+        for (int i = 0; i < b; i++) {
+            B.add(Integer.parseInt(st.nextToken()));
         }
 
+        A.removeAll(B);
 
-        Set<Integer> set = new HashSet<>();
-        StringBuilder sb = new StringBuilder();
-
-        for (int i = 0; i < A.length; i++) {
-            int t = 0;
-            for (int j = 0; j < B.length; j++) {
-                if (A[i] == B[j]) {
-                    t = 0;
-                    break;
-                } else if (A[i] != B[j]) {
-                    t++;
-                }
-            }
-            if (t > 0) {
-                set.add(A[i]);
-                sb.append(A[i]).append(" ");
-            }
-        }
-
-        if (set.isEmpty()) {
+        if (A.isEmpty()) {
             System.out.println(0);
-
         } else {
-            System.out.println(set.size());
-            System.out.println(sb);
+            TreeSet<Integer> treeSet = new TreeSet<>(A);
+
+            System.out.println(A.size());
+            for (int c : treeSet) {
+                System.out.print(c + " ");
+            }
         }
     }
 }
