9c03b4aff6f4830cabccdfa01da85a4acdbbd8aa
Umjiseung <127853946+Umjiseung@users.noreply.github.com>
2024-10-02 00:51:00 +0900

    s_1158

--- src/baekjoon/silver/s_1158/Main.java
+++ src/baekjoon/silver/s_1158/Main.java
@@ -9,24 +9,27 @@ public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
-        int n = Integer.parseInt(st.nextToken());
-        int k = Integer.parseInt(st.nextToken());
+        int N = Integer.parseInt(st.nextToken());
+        int K = Integer.parseInt(st.nextToken());
+
         Queue<Integer> queue = new LinkedList<>();
-
-        for (int i = 1; i <= n; n++) {
-            queue.add(i);
+        for (int i = 0; i < N; i++) {
+            queue.add(i+1);
         }
 
+        StringBuilder sb = new StringBuilder();
+        System.out.print("<");
         while (!queue.isEmpty()) {
-
+            for (int i = 0; i < K-1; i++) {
+                queue.add(queue.remove());
+            }
+            if (queue.size() == 1) {
+                sb.append(queue.remove() + ">");
+                break;
+            }
+            sb.append(queue.remove()).append(", ");
         }
-
-
-
-
+        System.out.println(sb);
     }
-
-
 }
