bf9026423a9426998d84404e81f6e506a4eeb5f1
Umjiseung <127853946+Umjiseung@users.noreply.github.com>
2025-08-19 09:22:58 +0900

    one_12934

---
+++ src/programmers/one_12934/Main.java
@@ -0,0 +1,23 @@
+package programmers.one_12934;
+// https://school.programmers.co.kr/learn/courses/30/lessons/12934
+public class Main {
+    public static void main(String[] args) {
+
+    }
+}
+
+class Solution {
+    public long solution(long n) {
+        long answer = 0;
+
+        double ja = Math.sqrt(n);
+
+        if (ja > (long) ja) {
+            answer = -1;
+        } else {
+            answer = (long) Math.pow(ja + 1, 2);
+        }
+
+        return answer;
+    }
+}
