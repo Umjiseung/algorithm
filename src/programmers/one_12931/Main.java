6048cb5a40bdcdc1b90dfc3a294d1a6e4923447d
Umjiseung <127853946+Umjiseung@users.noreply.github.com>
2025-08-20 09:08:41 +0900

    one_12931

---
+++ src/programmers/one_12931/Main.java
@@ -0,0 +1,26 @@
+package programmers.one_12931;
+
+public class Main {
+    public static void main(String[] args) {
+
+    }
+}
+
+class Solution {
+    public int solution(int n) {
+        int answer = 0;
+
+        String str = String.valueOf(n);
+
+        for (int i = 0; i < str.length(); i++) {
+            int a = Integer.parseInt(String.valueOf(str.charAt(i)));
+            answer += a;
+        }
+
+        return answer;
+    }
+}
+
+
+
+
